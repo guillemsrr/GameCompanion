3c580c5ac9183f42ddeb6ffd8df2ed21d36b7b29
bernatriu <bernatriupuyal@enti.cat>
2019-01-13 00:27:40 +0100

    pujant llibreries per compilar better

--- GameCompanion_Project/app/src/main/java/bernatriu/gotcompanion/Fragments/CharactersFragment.kt
+++ GameCompanion_Project/app/src/main/java/bernatriu/gotcompanion/Fragments/CharactersFragment.kt
@@ -1,11 +1,11 @@
 
 package bernatriu.gotcompanion.Fragments
 
-import android.app.Fragment
+
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
-import android.support.v7.app.Fragment
+import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.LayoutInflater
